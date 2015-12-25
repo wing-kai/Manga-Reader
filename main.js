5bfce9fb37fad2875a8812ec746c1696f21f477b
chenyongjia <cyj9385@hotmail.com>
2015-12-26 02:44:43 +0800

    基础文件

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const electron = require('electron');
+
+const ipc = electron.ipcMain;
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const fs = require("fs");
+
+electron.crashReporter.start();
+
+let mainWindow;
+let appReady = new Promise(function(resolve) {
+    app.on('ready', resolve);     
+});
+
+appReady.then(function() {
+    mainWindow = new BrowserWindow({
+        center: true
+    });
+    
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
